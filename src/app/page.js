10a22bc29ddaf9993906fdc3f2374b67b50f7b6d
pablobernalmoreno <pbm1998@hotmail.com>
2025-06-12 12:55:49 -0500

    Add talesof

--- src/app/page.js
+++ src/app/page.js
@@ -38,32 +38,39 @@ const myMangas = [
     id: 3,
   },
   {
+    title: "Tales of Demons and Gods",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm8Xo_FtYRd6Ce_ZLx-lzZsqwjx_XwIzIUOw&s",
+    page: "https://manhuaplus.com/manga/tales-of-demons-and-gods01/",
+    id: 4,
+  },
+  {
     title: "Ancient Magus Bride",
     image:
       "https://cdnx.jumpseller.com/spookyhouse/image/2030700/resize/640/500?1649814341",
     page: "https://www.natomanga.com/manga/mahou-tsukai-no-yome",
-    id: 4,
+    id: 5,
   },
   {
     title: "Goblin Slayer",
     image:
       "https://images.cdn3.buscalibre.com/fit-in/360x360/1f/3f/1f3f8841403c7340c7320bc5288ee3fb.jpg",
     page: "https://www.natomanga.com/manga/goblin-slayer",
-    id: 5,
+    id: 6,
   },
   {
     title: "Record of Ragnarok",
     image:
       "https://preview.redd.it/tlybkmyizdo81.jpg?width=1080&crop=smart&auto=webp&s=d69c12d833493f33546367a68476d78ac8b1cdcc",
     page: "https://www.natomanga.com/manga/record-of-ragnarok",
-    id: 6,
+    id: 7,
   },
   {
     title: "Mieruko Chan",
     image:
       "https://upload.wikimedia.org/wikipedia/en/e/e1/Mieruko-chan_volume_1_cover.jpg",
     page: "https://www.natomanga.com/manga/mieruko-chan",
-    id: 7,
+    id: 8,
   },
 ];
 
