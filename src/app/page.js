39c7116e857d6f5d88504e39a71c1f07749e69cd
Pablo Bernal Moreno <pbm1998@hotmail.com>
2026-03-26 15:44:04 -0500

    feat: order mangas by id ascending

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,8 @@ export const dynamic = "force-dynamic";
 export default async function Home() {
   const { data: mangas, error } = await supabase
     .from("mangas")
-    .select("id, name, image, link, description");
+    .select("id, name, image, link, description")
+    .order("id", { ascending: true });
 
   if (error) throw new Error(error.message);
 
